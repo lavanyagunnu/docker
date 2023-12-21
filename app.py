ec446c238b4cc2553db576e927be82aeb9d1f8d1
lavanya gunnu <lavanyagunnu01@gmail.com>
2023-12-21 15:48:09 +0000

    added navbar and bootstrap

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World! This is Lavanya here'
+def home():
+    return render_template('home.html')
 
+@app.route('/sketches')
+def sketches():
+    return render_template('sketches.html')
 
-@app.route('/problems')
-def problems():
-    return 'This is my list of problems'
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
